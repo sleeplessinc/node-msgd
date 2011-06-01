088d5251de10bfa4458db95067e3e0bbc73c058b
Joe Hitchens <joe@sleepless.com>
2011-06-01 09:44:45 -0700

    -

--- msgd.js
+++ msgd.js
@@ -2,38 +2,37 @@
 var http = require("http")
 
 var fail = function(res, err) {
-	var msgOut = err.message+"\n"
+	var msg = err.message+"\n"
 	res.writeHead(500, {
 		"Content-Type": "text/plain",
-		"Content-Length": msgOut.length,
+		"Content-Length": msg.length,
 	})
-	res.end(msgOut)
+	res.end(msg)
 }
 
 exports.createServer = function(msgHandler) {
 
 	return http.createServer(function(req, res) {
-		var msgIn = ""
+		var msg = ""
 		req.setEncoding("utf8")
 		if(req.method != "POST") {
 			fail(res, new Error("BAD METHOD: "+req.method))
 		}
 		else { 
 			req.on("data", function(d) {
-				msgIn += d
+				msg += d
 			})
 			req.on("error", function(e) {
 				fail(res, e)
 			})
 			req.on("end", function() {
 				try {
-					msgHandler(msgIn, function(msg) {
-						var msgOut = msg
+					msgHandler(msg, function(msg) {
 						res.writeHead(200, {
 							"Content-Type": "text/plain",
-							"Content-Length": msgOut.length,
+							"Content-Length": msg.length,
 						})
-						res.end(msgOut)
+						res.end(msg)
 					})
 				}
 				catch(e) {
